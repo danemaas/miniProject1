50d8425f07e143e424448c006f11b64abefda643
danemaas <danemaas16@gmail.com>
2023-09-13 08:43:40 +0800

    added and remove some data

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const cards = document.querySelectorAll('.product-box');
+
+[...cards].forEach( (card) => {
+    card.addEventListener('mouseover', function() {
+        card.classList.add('is-hover');
+    })
+
+    card.addEventListener('mouseleave', function() {
+        card.classList.remove('is-hover');
+    })
+})
