53fe277845ce41d23ebb15f110e3c69bd3a36234
danemaas <danemaas16@gmail.com>
2023-09-14 10:33:32 +0800

    added filter feature

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,31 @@ const cards = document.querySelectorAll('.product-box');
         card.classList.remove('is-hover');
     })
 })
+
+// ===== FILTER BUTTON =====
+const allFilterItems = document.querySelectorAll('.filter_prod');
+const allFilterBtns = document.querySelectorAll('.tag');
+
+allFilterBtns.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        showFilteredContent(btn);
+    });
+});
+
+function showFilteredContent(btn) {
+    allFilterItems.forEach((item) => {
+        if(item.classList.contains(btn.id)) {
+            resetActiveBtn();
+            btn.classList.add('active_btn');
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+
+function resetActiveBtn() {
+    allFilterBtns.forEach((btn) => {
+        btn.classList.remove('active_btn');
+    })
+}
